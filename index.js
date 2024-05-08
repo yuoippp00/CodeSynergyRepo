2b858964cc15af2df6b12caaa50b7b85ef972d7f
yuoippp00 <the.drumernam1311@outlook.com>
2024-05-08 11:13:13 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-function numIslands(grid) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const rows = grid.length;
-  const cols = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(i, j);
-      }
-    }
-  }
-  return islands;
-  function dfs(row, col) {
-    if (
-      row < 0 ||
-      row >= rows ||
-      col < 0 ||
-      col >= cols ||
-      grid[row][col] !== "1"
-    )
-      return;
-    grid[row][col] = "0";
-    dfs(row + 1, col);
-    dfs(row - 1, col);
-    dfs(row, col + 1);
-    dfs(row, col - 1);
-  }
-}
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
