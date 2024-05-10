9d1a128b1a681af834561b02bfaf94475becf079
yuoippp00 <the.drumernam1311@outlook.com>
2024-05-11 06:32:38 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const numbers = [1, 5, 2, 8];
-const max = Math.max(...numbers);
-console.log(max); // 8
+function minStack() {
+  this.stack = [];
+  this.minStack = [];
+}
+minStack.prototype.push = function (x) {
+  this.stack.push(x);
+  if (
+    this.minStack.length === 0 ||
+    x <= this.minStack[this.minStack.length - 1]
+  )
+    this.minStack.push(x);
+};
+minStack.prototype.pop = function () {
+  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
+    this.minStack.pop();
+};
+minStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+minStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
