66ee1d696e22aaac91da54022dce29a1911b3e5a
yuoippp00 <the.drumernam1311@outlook.com>
2024-05-13 15:14:05 +0700

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-    }
-    triangle.push(row);
-  }
-  return triangle;
-}
+const drop = (arr, n = 1) => arr.slice(n);
