78e0b2a5598a574e3d5987c43979a6b4662d8b60
yuoippp00 <the.drumernam1311@outlook.com>
2024-05-14 22:21:04 +0700

    start

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const drop = (arr, n = 1) => arr.slice(n);
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
+}
