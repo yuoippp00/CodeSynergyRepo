a9cfb5aeba5245a36e4fa0975c47b9936e447f9e
yuoippp00 <the.drumernam1311@outlook.com>
2024-05-16 00:24:21 +0700

    feat project

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-  if (left && right) return root;
-  return left ? left : right;
+function maximalRectangle(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const heights = Array(cols).fill(0);
+  let maxArea = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      heights[j] = matrix[i][j] === "1" ? heights[j] + 1 : 0;
+    }
+    maxArea = Math.max(maxArea, largestRectangleArea(heights));
+  }
+  return maxArea;
+  function largestRectangleArea(heights) {
+    const stack = [];
+    let maxArea = 0;
+    for (let i = 0; i <= heights.length; i++) {
+      while (
+        stack.length !== 0 &&
+        (i === heights.length || heights[i] < heights[stack[stack.length - 1]])
+      ) {
+        const height = heights[stack.pop()];
+        const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+        maxArea = Math.max(maxArea, height * width);
+      }
+      stack.push(i);
+    }
+    return maxArea;
+  }
 }
